9f293524ca74046e13d538e36d39ecf54e4249e0
Nair <nair.alic@gmail.com>
2018-06-12 10:00:16 +0200

    Added load balancing with multiple servers, working WELL !

--- docker-images/apache-reverse-proxy/templates/config-template.php
+++ docker-images/apache-reverse-proxy/templates/config-template.php
@@ -1,14 +1,27 @@
 <?php 
-    $STATIC_APP = getenv('STATIC_APP');
-    $DYNAMIC_APP = getenv('DYNAMIC_APP');
+    $STATIC_APP1 = getenv('STATIC_APP1');
+	$STATIC_APP2 = getenv('STATIC_APP2');
+    
+	$DYNAMIC_APP1 = getenv('DYNAMIC_APP1');
+    $DYNAMIC_APP2 = getenv('DYNAMIC_APP2');
 ?>
 
 <VirtualHost *:80>
     ServerName demo.res.ch
     
-    ProxyPass '/api/company/' 'http://<?php print "$DYNAMIC_APP" ?>/'
-    ProxyPassReverse '/api/company/' 'http://<?php print "$DYNAMIC_APP" ?>/'
+    <Proxy "balancer://companyset">
+        BalancerMember 'http://<?php print "$DYNAMIC_APP1"?>'
+        BalancerMember 'http://<?php print "$DYNAMIC_APP2" ?>'
+    </Proxy>
     
-    ProxyPass '/' 'http://<?php print "$STATIC_APP" ?>/'
-    ProxyPassReverse '/' 'http://<?php print "$STATIC_APP" ?>/' 
+    <Proxy "balancer://webserverset">
+        BalancerMember 'http://<?php print "$STATIC_APP1" ?>/'
+        BalancerMember 'http://<?php print "$STATIC_APP2" ?>/'
+    </Proxy>
+    
+    ProxyPass '/api/company/' 'balancer://companyset/'
+    ProxyPassReverse '/api/company/' 'balancer://companyset/'
+    
+    ProxyPass '/' 'balancer://webserverset/'
+    ProxyPassReverse '/' 'balancer://webserverset/'	 
 </VirtualHost>
