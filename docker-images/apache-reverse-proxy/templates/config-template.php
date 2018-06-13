636141df3eb1680e42c034ecd238fe310a29b8f7
Nair <nair.alic@gmail.com>
2018-06-13 14:02:50 +0200

    Added load balancing with sticky sessions, seems to work well!

--- docker-images/apache-reverse-proxy/templates/config-template.php
+++ docker-images/apache-reverse-proxy/templates/config-template.php
@@ -8,15 +8,17 @@
 
 <VirtualHost *:80>
     ServerName demo.res.ch
-    
+	
     <Proxy "balancer://company">
         BalancerMember 'http://<?php print "$DYNAMIC_APP1"?>'
         BalancerMember 'http://<?php print "$DYNAMIC_APP2" ?>'
     </Proxy>
     
+	Header add Set-Cookie "ROUTEID=.%{BALANCER_WORKER_ROUTE}e; path=/" env=BALANCER_ROUTE_CHANGED
     <Proxy "balancer://webserver">
-        BalancerMember 'http://<?php print "$STATIC_APP1" ?>/'
-        BalancerMember 'http://<?php print "$STATIC_APP2" ?>/'
+        BalancerMember 'http://<?php print "$STATIC_APP1" ?>/' route=1
+        BalancerMember 'http://<?php print "$STATIC_APP2" ?>/' route=2
+		ProxySet stickysession=ROUTEID
     </Proxy>
     
     ProxyPass '/api/company/' 'balancer://company/'
